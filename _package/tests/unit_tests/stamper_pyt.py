76bf9b7381a72956b07e8b7e7754662a814b45e3
Gage Larsen <glarsen@aquaveo.com>
2025-10-15 09:36:59 -0600

    fix typo

--- _package/tests/unit_tests/stamper_pyt.py
+++ _package/tests/unit_tests/stamper_pyt.py
@@ -23,7 +23,7 @@ class TestStamper(unittest.TestCase):
         )
         self.assertTrue(os.path.isdir(os.path.join(script_path, '..', '..', '..')))
         self.assertTrue(os.path.isdir(os.path.join(script_path, '..', '..', '..', '_package')))
-        self.assertTrue(os.path.isdir(os.path.join(script_path, '..', '..', '..', '_package', 'tests'))
+        self.assertTrue(os.path.isdir(os.path.join(script_path, '..', '..', '..', '_package', 'tests')))
         self.assertTrue(os.path.isdir(self.base_file_path),
                         f"Input file directory does not exist {self.base_file_path}")
         self.output_file_path = os.path.join(
